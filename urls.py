e4f366a0f1a2b7182b284a2384486caef23be13b
Corey <coreyswan@Hispaniola.local>
2011-04-14 20:18:04 -0400

    Now with index.

--- urls.py
+++ urls.py
@@ -2,9 +2,12 @@ from django.conf.urls.defaults import *
 
 from oshbestiary.models import Power, Creature
 
-urlpatterns = patterns('django.views.generic',
-    url(r'^$', 'simple.direct_to_template', { 'template': 'oshbestiary_index.html'}),
-    url(r'^(?P<slug>[-\w]+)/$', 'list_detail.object_detail', 
+urlpatterns = patterns('django.views.generic.list_detail',
+    url(r'^$', 'object_list', { 
+    	'queryset': Creature.objects.all(), 
+    	'template_name': 'oshbestiary_index.html',
+    	'template_object_name': 'creature'}),
+    url(r'^(?P<slug>[-\w]+)/$', 'object_detail', 
     {
     	'queryset': Creature.objects.all(), 
     	'template_name': 'creature_detail.html'
