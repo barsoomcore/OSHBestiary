cabd7ddcebd46640a9666ff3d2bcb9d630ed3e38
Corey <coreyswan@Hispaniola.local>
2011-12-03 15:41:24 -0500

    Creating "View on site" links in list page.

--- admin.py
+++ admin.py
@@ -9,11 +9,16 @@ class PowerAdmin(admin.ModelAdmin):
 		obj.save()
 
 class CreatureAdmin(admin.ModelAdmin):
-	list_display = ('name', 'owner', 'description', 'tags')
+	list_display = ('name', 'owner', 'description', 'tags', 'public_link')
 	
 	def save_model(self, request, obj, form, change):
 		obj.owner = request.user
 		obj.save()
+	
+	def public_link(self, obj):
+		return ("<a href='" + obj.get_absolute_url() + "'>View</a>")
+	public_link.short_description=""
+	public_link.allow_tags = True
 
 admin.site.register(Power, PowerAdmin)
 admin.site.register(Creature, CreatureAdmin)
